915c6f59193e39a62e082f755444df83b105709f
Henning Dieterichs <henning.dieterichs@live.de>
2020-07-23 16:07:00 +0200

    Update

--- lib/src/EditorFactories/NumberEditorFactory.tsx
+++ lib/src/EditorFactories/NumberEditorFactory.tsx
@@ -6,39 +6,39 @@ import { observer } from "mobx-react";
 import { action } from "mobx";
 
 export class NumberEditorFactory extends EditorFactory {
-    public getEditors(node: Node, factory: EditorFactory): Editor[] {
-        if (node.kind !== "primitive" || node.actualType.kind !== "number") {
-            return [];
-        }
+	public getEditors(node: Node, factory: EditorFactory): Editor[] {
+		if (node.kind !== "primitive" || node.actualType.kind !== "number") {
+			return [];
+		}
 
-        return [
-            {
-                isSingleLine: true,
-                priority: 100,
-                render: () => <NumberEditor node={node} />
-            }
-        ];
-    }
+		return [
+			{
+				isSingleLine: true,
+				priority: 100,
+				render: () => <NumberEditor node={node} />,
+			},
+		];
+	}
 }
 
 @observer
 class NumberEditor extends React.Component<{
-    node: PrimitiveNode;
+	node: PrimitiveNode;
 }> {
-    @action.bound
-    handleInputChange(value: number): void {
-        this.props.node.value = value;
-    }
+	@action.bound
+	handleInputChange(value: number): void {
+		this.props.node.value = value;
+	}
 
-    render() {
-        const { node } = this.props;
-        return (
-            <div className="component-NumberEditor">
-                <NumericInput
-                    value={node.value as number}
-                    onValueChange={this.handleInputChange}
-                />
-            </div>
-        );
-    }
+	render() {
+		const { node } = this.props;
+		return (
+			<div className="component-NumberEditor">
+				<NumericInput
+					value={node.value as number}
+					onValueChange={this.handleInputChange}
+				/>
+			</div>
+		);
+	}
 }
